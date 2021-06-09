2e564baaabd71f509819754f4392d05b1dea9176
Conor Fayle <conor@lectrefy.com>
2021-06-09 16:49:50 +0100

    ReactExoplayerView.java - remove flushOrReinitializeCodec()

--- android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
+++ android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
@@ -835,7 +835,6 @@ class ReactExoplayerView extends FrameLayout implements
                         errorString = getResources().getString(R.string.error_no_decoder,
                                 decoderInitializationException.mimeType);
                     }
-                    MediaCodecRenderer.flushOrReinitializeCodec();
                 } else {
                     errorString = getResources().getString(R.string.error_instantiating_decoder,
                             decoderInitializationException.decoderName);
