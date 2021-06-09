fbeddb5414ab157b8aeef8308dc05f1d7a139f7d
Conor Fayle <conor@lectrefy.com>
2021-06-09 16:51:04 +0100

    ExoPlayerView.java - view.setAlpha(0)

--- android-exoplayer/src/main/java/com/brentvatne/exoplayer/ExoPlayerView.java
+++ android-exoplayer/src/main/java/com/brentvatne/exoplayer/ExoPlayerView.java
@@ -94,6 +94,7 @@ public final class ExoPlayerView extends FrameLayout {
 
     private void updateSurfaceView() {
         View view = useTextureView ? new TextureView(context) : new SurfaceView(context);
+        view.setAlpha(0);
         view.setLayoutParams(layoutParams);
 
         surfaceView = view;
